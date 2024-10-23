65cfd8a810bb7bef4882cf6d2b4e3c9215f8112a
Maira-Atif <mairaatifriaz@gmail.com>
2024-10-22 21:38:18 -0700

    Add files via upload

---
+++ DSALabtask1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dsalabtask1;
+
+/**
+ *
+ * @author 123
+ */
+public class DSALabtask1 {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        int n = 10; 
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println("The sum of the first " + n + " natural numbers is: " + sum);
+    }
+
+    }
+
